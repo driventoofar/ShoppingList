41a3ead73b441e9effa0a8d2d74042845d244f94
Andrew Van Orden <driventoofar@hotmail.com>
2020-06-05 15:21:25 -0400

    added list and add item.

---
+++ ShoppingList.Blazor/Models/ListItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Blazor.Models
+{
+    public class ListItem
+    {
+        [Required]
+        public string Item { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
