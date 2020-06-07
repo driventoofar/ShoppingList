3f5fd94afa57e482bc62bf7a80cb0b15936f4b2f
Andrew Van Orden <driventoofar@hotmail.com>
2020-06-07 16:17:19 -0400

    add api project

---
+++ ShoppingList.API/Controllers/ShoppingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingList.API.Models;
+
+namespace ShoppingList.API.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly List<ListItem> _items = null;
+
+        public ShoppingController()
+        {
+            _items = new List<ListItem>()
+            {
+                new ListItem
+                {
+                    Id = Guid.NewGuid(),
+                    Item = "Apple",
+                    Name = "Andrew"
+                }
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<ListItem> GetItems()
+        {
+            return _items;
+        }
+    }
+}
---
+++ ShoppingList.API/Models/ListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.API.Models
+{
+    public class ListItem
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Item { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
