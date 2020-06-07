181836fb74b29d4c2132c2aa4f3a6f1465040402
Andrew Van Orden <driventoofar@hotmail.com>
2020-06-07 16:17:34 -0400

    update blazor project

--- ShoppingList.Blazor/Models/ListItem.cs
+++ ShoppingList.Blazor/Models/ListItem.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShoppingList.Blazor.Models
 {
     public class ListItem
     {
+        public Guid Id { get; set; }
+
         [Required]
         public string Item { get; set; }
 
